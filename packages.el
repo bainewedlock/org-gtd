b655f928104e20a8e5d5fc6f88073211d26e9b5d
Baine Wedlock <bainewedlock@gmail.com>
2020-01-09 12:10:07 +0100

    * removed %a from templates (confidentiality) * moved habit timestmp

--- packages.el
+++ packages.el
@@ -551,11 +551,11 @@ so change the default 'F' binding in the agenda to allow both"
   ;; meetings, and org-protocol
   (setq org-capture-templates
         (quote (("t" "todo" entry (file "~/git/org/refile.org")
-                 "* TODO %?\n%U\n%a\n" :clock-in t :clock-resume t)
+                 "* TODO %?\n%U\n" :clock-in t :clock-resume t)
                 ("r" "respond" entry (file "~/git/org/refile.org")
-                 "* NEXT Respond to %:from on %:subject\nSCHEDULED: %t\n%U\n%a\n" :clock-in t :clock-resume t :immediate-finish t)
+                 "* NEXT Respond to %:from on %:subject\nSCHEDULED: %t\n%U\n" :clock-in t :clock-resume t :immediate-finish t)
                 ("n" "note" entry (file "~/git/org/refile.org")
-                 "* %? :NOTE:\n%U\n%a\n" :clock-in t :clock-resume t)
+                 "* %? :NOTE:\n%U\n" :clock-in t :clock-resume t)
                 ("j" "Journal" entry (file+datetree "~/git/org/diary.org")
                  "* %?\n%U\n" :clock-in t :clock-resume t)
                 ("w" "org-protocol" entry (file "~/git/org/refile.org")
@@ -565,9 +565,9 @@ so change the default 'F' binding in the agenda to allow both"
                 ("p" "Phone call" entry (file "~/git/org/refile.org")
                  "* PHONE %? :PHONE:\n%U" :clock-in t :clock-resume t)
                 ("h" "Habit" entry (file "~/git/org/refile.org")
-                 "* NEXT %?\n%U\n%a\nSCHEDULED: %(format-time-string \"%<<%Y-%m-%d %a .+1d/3d>>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n")
+                 "* NEXT %?\nSCHEDULED: %(format-time-string \"%<<%Y-%m-%d %a .+1d/3d>>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n%U\n")
                 ("s" "someday/maybe" entry (file+olp "~/git/org/someday.org" "Someday")
-                 "* TODO %?\n%U\n%a\n" :clock-in t :clock-resume t))))
+                 "* TODO %?\n%U\n" :clock-in t :clock-resume t))))
 
   ;; Remove empty LOGBOOK drawers on clock out
   (defun bh/remove-empty-drawer-on-clock-out ()
