53ee332700e3b39d252ebde7d75489aa2538305f
Baine Wedlock <bainewedlock@gmail.com>
2020-07-13 13:10:01 +0200

    * packages.el: extra zeilenumbruch nach capture template

--- packages.el
+++ packages.el
@@ -571,7 +571,7 @@ so change the default 'F' binding in the agenda to allow both"
                 ("s" "someday/maybe" entry (file+headline "~/git/org/someday.org" "Someday")
                  "* TODO %?\n%U\n" :clock-in t :clock-resume t)
                 ("d" "daily jour fixe" entry (file+headline org-default-notes-file "Tasks")
-                 "* TODO Daily Jour Fixe %?%t [/]\n- [ ] Agenda ausfüllen\n- [ ] Abgleich mit Kanban Board (passt \"Aktiv / als nächstes\"?)\n- [ ] Abgleich mit Prio (passt \"Aktiv / als nächstes\"?)\n*** Prio:\n*** Fertig\n*** Aktiv / als nächstes:\n*** Hürden:\n*** News:\n"))))
+                 "* TODO Daily Jour Fixe %?%t [/]\n- [ ] Agenda ausfüllen\n- [ ] Abgleich mit Kanban Board (passt \"Aktiv / als nächstes\"?)\n- [ ] Abgleich mit Prio (passt \"Aktiv / als nächstes\"?)\n*** Prio:\n*** Fertig\n*** Aktiv / als nächstes:\n*** Hürden:\n*** News:\n\n"))))
 
   ;; Remove empty LOGBOOK drawers on clock out
   (defun bh/remove-empty-drawer-on-clock-out ()
